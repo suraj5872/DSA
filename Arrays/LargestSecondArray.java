b6e1939d32d6833c316cfdbdbb5c33cb3c026c86
suraj5872 <sksuraj5872@gmail.com>
2024-07-25 00:05:44 +0530

    Second Largest Element in array

--- Arrays/LargestSecondArray.java
+++ Arrays/LargestSecondArray.java
@@ -1,33 +1,33 @@
-Class LargestSecondArray
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestSecondArray
 {
 
-	public int print2largest(List<Integer> arr) {
-			// Code Here
-			if (arr.isEmpty() || arr.size() < 2) {
-				return -1;
-			}
+    public static int print2largest(List<Integer> arr) {
+        if (arr.isEmpty() || arr.size() < 2) {
+            return -1;
+        }
 
-			// Use a Set to track distinct elements
-			Set<Integer> distinctElements = new HashSet<>();
-			int first = Integer.MIN_VALUE;
-			int second = Integer.MIN_VALUE;
+        // Use a Set to track distinct elements
+        //Set<Integer> distinctElements = new HashSet<>();
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
 
-			for (int num : arr) {
-				if (num > first) {
-					second = first;
-					first = num;
-				} else if (num > second && num < first) {
-					second = num;
-				}
-			}
-			// If we don't have a valid second largest element
-			return second == Integer.MIN_VALUE ? -1 : second;
-		}
-	}
-	public static void main(String[] args) {
-        List list = Arrays.aslist(10,0,3,0,2,0,7);
-		print2largest(list)
-        System.out.println();
-        
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num < first) {
+                second = num;
+            }
+        }
+        // If we don't have a valid second largest element
+        return second == Integer.MIN_VALUE ? -1 : second;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,0,3,0,2,0,7);
+        System.out.println(print2largest(list));
     }
 }
