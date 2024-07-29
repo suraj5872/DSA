9f3466c0d484a80517b6fe3e2c1e258fd17a5b91
suraj5872 <sksuraj5872@gmail.com>
2024-07-30 00:03:42 +0530

    MoveZerosToEnd

--- Arrays/MoveZerosToEnd.java
+++ Arrays/MoveZerosToEnd.java
@@ -1,29 +1,19 @@
-public class MoveZerosToEnd { public static int[] moveZeros(int n, int []a) {
-    int j = -1;
-    //place the pointer j:
-    for (int i = 0; i < n; i++) {
-        if (a[i] == 0) {
-            j = i;
-            break;
+public class MoveZerosToEnd {
+
+    public static int[] moveZeros(int n, int []a){
+        int j=0;
+        for (int i = 1; i < n; i++) {
+            if (a[i]!=0){
+                a[j] =a[i];
+                j++;
+            }
         }
-    }
-
-    //no non-zero elements:
-    if (j == -1) return a;
-
-    //Move the pointers i and j
-    //and swap accordingly:
-    for (int i = j + 1; i < n; i++) {
-        if (a[i] != 0) {
-            //swap a[i] & a[j]:
-            int tmp = a[i];
-            a[i] = a[j];
-            a[j] = tmp;
+        while (j<n){
+            a[j]=0;
             j++;
         }
+        return a;
     }
-    return a;
-}
 
     public static void main(String[] args) {
         int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
@@ -32,7 +22,7 @@ public class MoveZerosToEnd { public static int[] moveZeros(int n, int []a) {
         for (int i = 0; i < n; i++) {
             System.out.print(ans[i] + " ");
         }
-        System.out.println("");
+        System.out.println(" ");
     }
 }
 
