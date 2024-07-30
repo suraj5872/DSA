b7a122334db9526d8e0aaa7bf87d0aa1f4c07a40
suraj5872 <sksuraj5872@gmail.com>
2024-07-30 21:48:43 +0530

    LinearSearch

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    static int linearSearch(int[] a,int target){
+        for (int i = 0; i <a.length ; i++) {
+            if (a[i] ==target){
+                System.out.println(+a[i]+" is present at "+i);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2,4,5,9,7,12,16};
+        System.out.println(linearSearch(a,7));
+    }
+}
